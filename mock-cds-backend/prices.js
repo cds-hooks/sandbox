8a60783e92046d3af3cac3fe18ec524ace36a770
Josh Mandel <joshua.mandel@childrens.harvard.edu>
2015-09-12 14:18:54 -0400

    Begin extracting pricing service data

---
+++ mock-cds-backend/prices.js
@@ -0,0 +1,89 @@
+var isArray = require('util').isArray
+var getIn = require('./utils').getIn
+var priceTable = require('./rxnorm-prices')
+
+
+module.exports = function(indata, cb) {
+  cb(null, recommend(indata));
+}
+
+// recommending means returning a set of "card"s, each with a summary, set of suggestions and a set of links.
+// recommending can, alternatively, return a single "decision" indicating a user-approved choice.
+
+function recommend(data) {
+  var lowerDose = getIn(data, 'content')[0]["resource"];
+  if (!lowerDose.medicationCodeableConcept){
+    return {}
+  }
+
+  return {
+    "resourceType": "Parameters",
+    "parameter": [
+      {
+        "name": "card",
+        "part": [{
+          "name": "summary",
+          "valueString": "JNC 8 guidelines may apply",
+        }, {
+          "name": "link",
+          "part": [{
+            "name": "label",
+            "valueString": "Tailor therapy with JNC Pro"
+          }, {
+            "name": "url",
+            "valueUri": "https://www.cms.gov/Newsroom/MediaReleaseDatabase/Fact-sheets/2015-Fact-sheets-items/2015-04-30.html"
+          }]
+        }]
+      },{
+        "name": "card",
+        "part": [{
+          "name": "summary",
+          "valueString": "Dose is high (>99.9th percentile)",
+        }, {
+          "name": "suggestion",
+          "part": [{
+            "name": "label",
+            "valueString": "5 mg daily"
+          }, {
+            "name": "alternative",
+            "resource": lowerDose
+          }]
+        }, {
+          "name": "suggestion",
+          "part": [{
+            "name": "label",
+            "valueString": "10 mg daily"
+          }, {
+            "name": "alternative",
+            "resource": lowerDose
+          }]
+        }, {
+          "name": "link",
+          "part": [{
+            "name": "label",
+            "valueString": "View prescribing trends"
+          }, {
+            "name": "url",
+            "valueUri": "https://www.cms.gov/Newsroom/MediaReleaseDatabase/Fact-sheets/2015-Fact-sheets-items/2015-04-30.html"
+          }]
+        }]
+      },{
+        "name": "card",
+        "part": [{
+          "name": "summary",
+          "valueString": "Generic lisinopril saves $35 copay",
+        }, {
+          "name": "suggestion",
+          "part": [{
+            "name": "label",
+            "valueString": "lisinopril"
+          }, {
+            "name": "alternative",
+            "resource": lowerDose
+          }]
+        }]
+      }, 
+    ]
+  }
+}
+
