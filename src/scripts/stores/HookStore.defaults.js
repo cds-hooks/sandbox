16c96f7ea8c3a9ae539f762d5ae1bd72a2a12921
Josh Mandel <jmandel@med.harvard.edu>
2016-04-28 00:22:57 -0400

    Fix default hooks

--- src/scripts/stores/HookStore.defaults.js
+++ src/scripts/stores/HookStore.defaults.js
@@ -2,10 +2,10 @@ var url = runtime.CDS_HOOKS_URL
 
 export default {
   "cms-price-check": {
-    "id": "cms-price-check",
     "enabled": true,
+    "id": url + "/cds-services/cms-price-check",
     "url": url + "/cds-services/cms-price-check",
     "name": "CMS Price Check",
-    "activity": "medication-prescribe"
+    "hook": "medication-prescribe"
   }
 }
