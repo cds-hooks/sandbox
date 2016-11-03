8b0dd29faabf6b5186997a4f612f7840515bf241
nschwertner <nikolai.schwertner@childrens.harvard.edu>
2016-11-03 16:52:26 -0400

    A couple improvements to the test harness (#12)
    * Display patient name and ID in all views
    
    * Fix patient ID selector bug
    
    * Add ability to hide FHIR view text to reduce distraction to non-developers during demos
    
    * Hide FHIR panel when disabled
    
    * Correct style assignments for FHIR panels

--- src/scripts/components/PatientViewActivity.js
+++ src/scripts/components/PatientViewActivity.js
@@ -13,13 +13,15 @@ const PatientViewActivity = React.createClass({
     var patient = this.props.all.getIn(['fhirServer', 'patient'])
     var name = "name";
     var dob = "dob"
-    if (patient) name = patient.name[0].given.join(" " ) + " " + patient.name[0].family.join(" " )
+    var pid = "pid"
+    if (patient) name = patient.name[0].given.join(" ") + " " + patient.name[0].family.join(" ")
+    if (patient) pid = patient.id
     if (patient) dob = patient.birthDate
     return <div id="main" className="app-main">
       <div className="order-entry patient-view">
         <h1 className="view-title">Patient View</h1>
         <h2>{name}</h2>
-        <p><b>Birthdate: </b> {dob}</p>
+        <p><strong>ID: </strong> {pid} <strong>Birthdate: </strong> {dob}</p>
         <div className="decision-spacer"></div>
         <Cards className="card-holder" decisions={this.props.all.get('decisions')} />
       </div>
