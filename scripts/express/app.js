f87c8b18a727e0c113f98b65202e7d9dd601e16e
Zach Plata <plata.zach@gmail.com>
2017-07-26 23:10:25 -0500

    Add heroku config file to enable heroku deployments on PR builds. This will allow changes to be deployed automatically so those reviewing PRs can functionally test easily.

---
+++ scripts/express/app.js
@@ -0,0 +1,12 @@
+/* eslint-disable import/no-extraneous-dependencies */
+/* eslint-disable no-console */
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const port = process.env.PORT || 8081;
+// path to webpack built path
+const buildPath = path.join(__dirname, '../../build');
+
+app.use(express.static(buildPath));
+app.listen(port);
