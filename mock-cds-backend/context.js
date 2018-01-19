0ecfc95a153ecf131dca9101eac517583b905dd5
Zach Plata <zach.plata@cerner.com>
2018-01-19 10:46:44 -0600

    Reset master-2.0 to set base for Sandbox rewrite

--- mock-cds-backend/context.js
+++
@@ -1,9 +0,0 @@
-var _url = "";
-
-module.exports = setContext;
-
-function setContext(server) {
-  setContext.url = process.env.URL || server.url
-  console.log("env", process.env.URL)
-  console.log("server", server.url)
-}
--- preprocessor.js
+++
@@ -1,3 +0,0 @@
-// TODO: collapse to config
-// https://babeljs.io/docs/setup/#jest
-module.exports = require('babel-jest');
