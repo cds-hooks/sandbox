cds-hooks/legacy-sandbox

Add implementation for Change Patient and Change FHIR Server menu dropdown options

src/components/MainView/main-view.jsx
@@ -64,7 +64,9 @@ export class MainView extends Component {
       });
     });
     if (this.state.patientPrompt) this.setState({ patientPrompt: false });
-    const getServiceDefinitions = await retrieveDiscoveryServices();
+    const getServiceDefinitions = await retrieveDiscoveryServices().catch(() => {
+      this.props.setLoader(false);
+    });
     this.props.setLoader(false);
   }
 

cds-hooks/sandbox

src/components/MainView/main-view.jsx
@@ -59,7 +59,9 @@ export class MainView extends Component {
       });
     }));
     if (this.state.patientPrompt) this.setState({ patientPrompt: false });
-    await retrieveDiscoveryServices();
+    await retrieveDiscoveryServices().catch(() => {
+      this.props.setLoadingStatus(false);
+    });
     this.props.setLoadingStatus(false);
   }
 