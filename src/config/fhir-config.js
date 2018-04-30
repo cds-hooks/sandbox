363ec72cefbe90a66eba48432beac15d102caeb6
Zach Plata <plata.zach@gmail.com>
2018-04-30 14:05:12 -0500

    Add fhirAuthorization field to service request if applicable. Switch current production client ID for Sandbox to old Sandbox production client ID. Fixes #22

--- src/config/fhir-config.js
+++ src/config/fhir-config.js
@@ -1,5 +1,5 @@
 // CDS Hooks DSTU2 HSPC Sandbox - https://sandbox.cds-hooks.org/launch.html
-export const productionClientId = '36600f19-a0de-4dcc-bf04-db30d8b09e8f';
+export const productionClientId = '48163c5e-88b5-4cb3-92d3-23b800caa927';
 
 // CDS Hooks DSTU2 HSPC Sandbox - http://localhost:8080/launch.html
 export const localhostClientId = '99407c8a-3069-41b1-af07-2612bc1f99de';
