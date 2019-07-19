8a089c0f7d89079657d468c433d2c2fa3a4e0d4f
Josh Mandel <jmandel@med.harvard.edu>
2019-07-19 15:02:03 -0500

    Use .payload field in postMessage body

--- tests/components/Pama/pama.test.js
+++ tests/components/Pama/pama.test.js
@@ -1,9 +1,10 @@
-import React from 'react';
-import { mount, shallow } from 'enzyme';
-import { Provider } from 'react-redux';
-import configureStore from 'redux-mock-store';
+import React from "react";
+import { mount, shallow } from "enzyme";
+import { Provider } from "react-redux";
+import configureStore from "redux-mock-store";
+import 'core-js/fn/array/flat-map';
 
-describe('Pama component', () => {
+describe("Pama component", () => {
   let storeState;
   let wrapper;
   let pureComponent;
@@ -16,24 +17,27 @@ describe('Pama component', () => {
 
   function setup(state) {
     mockStore = mockStoreWrapper(state);
-    jest.setMock('../../../src/retrieve-data-helpers/service-exchange', mockSpy);
-    ConnectedView = require('../../../src/components/Pama/pama').default;
-    Pama = require('../../../src/components/Pama/pama')['Pama'];
+    jest.setMock(
+      "../../../src/retrieve-data-helpers/service-exchange",
+      mockSpy
+    );
+    ConnectedView = require("../../../src/components/Pama/pama").default;
+    Pama = require("../../../src/components/Pama/pama")["Pama"];
 
-    let component = <ConnectedView store={mockStore}/>;
+    let component = <ConnectedView store={mockStore} />;
     wrapper = shallow(component);
     pureComponent = wrapper.find(Pama);
   }
 
   beforeEach(() => {
     storeState = {
-        pama: {
-            serviceRequest: {
-                code: 1,
-                reasonCode: 2
-            },
-            pamaRating: "appropriate"
-        }
+      pama: {
+        serviceRequest: {
+          code: 1,
+          reasonCode: 2
+        },
+        pamaRating: "appropriate"
+      }
     };
     mockSpy = jest.fn();
     setup(storeState);
@@ -43,9 +47,51 @@ describe('Pama component', () => {
     jest.resetModules();
   });
 
-  it('matches props passed down from Redux decorator', () => {
-    expect(pureComponent.prop('pamaRating')).toEqual(storeState.pama.pamaRating);
-    expect(pureComponent.prop('serviceRequest')).toEqual(storeState.pama.serviceRequest);
+  it("matches props passed down from Redux decorator", () => {
+    expect(pureComponent.prop("pamaRating")).toEqual(
+      storeState.pama.pamaRating
+    );
+    expect(pureComponent.prop("serviceRequest")).toEqual(
+      storeState.pama.serviceRequest
+    );
   });
 
+  it("Handles onMessage payloads correctly", () => {
+    const data = {
+      messageType: "scratchpad.update",
+      payload: {
+        resource: {
+          resourceType: "ServiceRequest",
+          id: "example-request-id",
+          code: {
+            coding: [
+              {
+                system: "http://www.ama-assn.org/go/cpt",
+                code: "72133"
+              }
+            ]
+          },
+          extension: [
+            {
+              url: "http://fhir.org/argonaut/Extension/pama-rating",
+              valueCodeableConcept: {
+                coding: [
+                  {
+                    system: "http://fhir.org/argonaut/CodeSystem/pama-rating",
+                    code: "no-criteria-apply"
+                  }
+                ]
+              }
+            }
+          ]
+        }
+      }
+    };
+
+    const onMessage = require("../../../src/components/Pama/pama").pamaTriggerHandler.onMessage
+    const dispatch = jest.fn()
+    onMessage({ data, dispatch })
+    expect(dispatch).toHaveBeenCalled()
+
+  });
 });
