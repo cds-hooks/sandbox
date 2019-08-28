fc652a4a615772517f8a00537285e152bc34edcd
Josh Mandel <jmandel@med.harvard.edu>
2019-08-28 18:18:11 -0500

    Uglify -> Terser

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,9 +1,13 @@
 const merge = require('webpack-merge');
 const webpack = require('webpack');
+const TerserPlugin = require('terser-webpack-plugin');
 const common = require('./webpack.config.common.js');
 
 module.exports = merge(common, {
   devtool: 'cheap-source-map',
   mode: 'production',
   plugins: [ ],
+  optimization: {
+    minimizer: [new TerserPlugin()],
+  },
 });
