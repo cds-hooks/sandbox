28b7fd2aad186386deba8d3691e6907914559f6f
Josh Mandel <jmandel@med.harvard.edu>
2019-08-28 10:59:11 -0500

    Fix build by removing uglify step

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -5,7 +5,6 @@ const common = require('./webpack.config.common.js');
 module.exports = merge(common, {
   devtool: 'source-map',
   plugins: [
-    new webpack.optimize.UglifyJsPlugin(),
     new webpack.DefinePlugin({
       'process.env.NODE_ENV': JSON.stringify('production'),
     }),
